2ce729fe4d72549cf9c0ffb06b86c2e40fe1ef4b
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2020-09-26 11:28:45 +0530

    The given inputs are sorted both in ascending and descending order.

---
+++ Asce_Dsce.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	printf("ASCENDING ORDER: ");
+	if((a==b)&&(b==c))
+	{
+			printf("%d %d %d",a,b,c);
+	}
+	else if(a>b && a>c)
+	{
+		if(b>c)
+		{
+			printf("%d %d %d",c,b,a);
+		}
+		else{
+			printf("%d %d %d",b,c,a);
+		}
+	}
+		else if(b>a&& b>c)
+	{
+		if(a>c)
+		{
+			printf("%d %d %d",c,a,b);
+		}
+		else{
+			printf("%d %d %d",a,c,b);
+		}
+	}
+	else{
+		if(c>b && c>a)
+		{
+			if(a>b)
+			{
+				printf("%d %d %d",b,a,c);
+			}
+			else{
+				printf("%d %d %d",a,b,c);
+			}
+		}
+	}
+	printf("\nDESCENDING ORDER:");
+	if((a==b)&&(b==c))
+	{
+			printf("%d %d %d",a,b,c);
+	}
+	else if(a>b && a>c)
+	{
+		if(b>c)
+		{
+			printf("%d %d %d",a,b,c);
+		}
+		else{
+			printf("%d %d %d",a,c,b);
+		}
+	}
+		else if(b>a&& b>c)
+	{
+		if(a>c)
+		{
+			printf("%d %d %d",b,a,c);
+		}
+		else{
+			printf("%d %d %d",b,c,a);
+		}
+	}
+	else{
+		if(c>b && c>a)
+		{
+			if(a>b)
+			{
+				printf("%d %d %d",c,a,b);
+			}
+			else{
+				printf("%d %d %d",c,b,a);
+			}
+		}
+	}
+	
+
+}
