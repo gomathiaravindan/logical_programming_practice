2c77e77966b02ee3274152c67e783cdd35e17b50
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-09 18:55:38 +0530

    Update del_ele.c

--- del_ele.c
+++ del_ele.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j,del_ele;
+	int n,i,j,del_ele,found=0;
 	scanf("%d",&n);
 	int a[n];
 	for(i=0;i<n;i++)
@@ -15,14 +15,20 @@ int main()
 	{
 		if(a[i]==del_ele)
 		{
+			found=1;
 			for(j=i;j<n;j++)
 			{
 				a[j]=a[j+1];
 			}
 		}
 	}
+	if(found==1)
+	{
 	for(i=0;i<n-1;i++)
 	{
 		printf("%d ",a[i]);
 	}
+	}
+	else
+		printf("Element not found!!");
 }
