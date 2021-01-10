98f16af337998845612088cf23208b300a85c21c
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-10 14:42:33 +0530

    Array is sorted using insertion sorting technique.

---
+++ insertion_sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,val;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=1;i<n;i++)
+	{
+		val = a[i];
+		j=i;
+		while(a[j-1] > val && j>=1)
+		{
+			a[j] = a[j-1];
+			j--;
+		}
+		a[j] = val;
+		
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
